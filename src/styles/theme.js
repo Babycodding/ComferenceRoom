6f3ddc63b118ed63d3edfcd83f4a1a91d5288c44
Nattawat Kaeosom <nattawatbd@gmail.com>
2019-07-03 16:53:49 +0700

    theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,5 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import deepOrange from '@material-ui/core/colors/purple';
+// import deepOrange from '@material-ui/core/colors/purple';
 
 export const theme = createMuiTheme({
     palette: {
