dfc52d445e9fcd2e445e929fd85ba7eb315ddd45
Nattawat Kaeosom <nattawatbd@gmail.com>
2019-07-08 18:35:21 +0700

    .

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import { ThemeProvider } from '@material-ui/styles';
 import * as serviceWorker from './serviceWorker';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { theme } from './styles/theme';
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 ReactDOM.render(
     <ThemeProvider theme={theme}>
+        <CssBaseline />
         <App/>
     </ThemeProvider>
 
